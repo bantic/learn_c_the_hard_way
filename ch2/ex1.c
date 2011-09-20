890112aa35f8adbd70195f6ab9f2328b5cdb1544
Cory Forsyth <cory.forsyth@gmail.com>
2011-09-20 15:38:44 -0400

    initial import

---
+++ ch2/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    puts("hello world.");
+    puts("hello world 2.");
+    puts("hello world 3.");
+    puts("hello world 4.");
+//    puts("hello world 5." + "next string");
+
+    return 0;
+}
